b4a65000663a331e09ca05a4a2739836101a64d4
Josue_Olivos <155196218+Josue-Olivos@users.noreply.github.com>
2026-06-13 12:43:19 -0600

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_Josue-Olivos-6bitcounter (
+module tt_um_JosueOlivos6bitcounter (
     input  wire [7:0] ui_in,
     output wire [7:0] uo_out,
     input  wire [7:0] uio_in,
