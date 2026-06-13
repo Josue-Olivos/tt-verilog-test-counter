e88066be19067ab382b84cd97690a40d6203dd8d
Josue_Olivos <155196218+Josue-Olivos@users.noreply.github.com>
2026-06-13 14:50:02 -0600

    Refactor test_counter to include ReadOnly trigger
    Added ReadOnly trigger to ensure correct output during test.

--- test/test.py
+++ test/test.py
@@ -1,9 +1,8 @@
 # SPDX-FileCopyrightText: © 2024 Tiny Tapeout
 # SPDX-License-Identifier: Apache-2.0
-
 import cocotb
 from cocotb.clock import Clock
-from cocotb.triggers import RisingEdge, Timer
+from cocotb.triggers import RisingEdge, Timer, ReadOnly
 
 
 @cocotb.test()
@@ -19,17 +18,16 @@ async def test_counter(dut):
 
     dut.rst_n.value = 0
     await Timer(1, unit="ns")
-
-    assert dut.uo_out.value.to_unsigned() == 0
+    assert (dut.uo_out.value.to_unsigned() & 0x3F) == 0
 
     await Timer(19, unit="ns")
     dut.rst_n.value = 1
 
     for expected in range(1, 20):
         await RisingEdge(dut.clk)
+        await ReadOnly()
         assert (dut.uo_out.value.to_unsigned() & 0x3F) == expected
 
     dut.rst_n.value = 0
     await Timer(1, unit="ns")
-
     assert (dut.uo_out.value.to_unsigned() & 0x3F) == 0
